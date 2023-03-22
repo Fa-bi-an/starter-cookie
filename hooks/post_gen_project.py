03c1254937cc19964ccbbe53beec061d9ddf3a00
fabian <dev@fabiannie.haus>
2023-03-22 22:45:23 +0100

    fix: made hook actually working

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,21 +8,22 @@ def main() -> None:
     print("********** POST GENERATION HOOK *************")
     
     if use_dvc == 'no':
-        print("Deleting DVC related files.")
-        delete_entity(Path(__file__).parent.parent / project / ".dvc")
-        delete_entity(Path(__file__).parent.parent / project / ".dvcignore")
+        print("Deleting DVC related files:")
+        delete_entity(Path(".dvc"))
+        delete_entity(Path(".dvcignore"))
         print("All DVC related files are deleted.")
              
 
 def delete_entity(entity:Path()) -> None:
+    print(entity)
     if entity.is_file():
         os.remove(entity)
     elif entity.is_dir():
         shutil.rmtree(entity)
     else:
-        print("File or directory does not exists.")
+        raise FileNotFoundError("File or directory does not exists.")
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()
 
