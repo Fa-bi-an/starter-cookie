5e366129cfc3bda81c8afadb7ae370ae632697c8
fabian <dev@fabiannie.haus>
2023-03-24 20:38:24 +0100

    style: black & isort

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,8 @@
-import sys, os, shutil
+import os
+import shutil
+import sys
 from pathlib import Path
+
 
 def main() -> None:
     use_dvc = "{{ cookiecutter.use_dvc }}"
@@ -7,19 +10,19 @@ def main() -> None:
     project = "{{ cookiecutter.project_slug }}"
     precommit = "{{ cookiecutter.use_precommit }}"
     print("********** POST GENERATION HOOK *************")
-    
-    if use_dvc == 'no':
+
+    if use_dvc == "no":
         print("Deleting DVC related files:")
         delete_entity(Path(".dvc"))
         delete_entity(Path(".dvcignore"))
         print("All DVC related files are deleted.")
-    if precommit == 'no':
+    if precommit == "no":
         print("Deleting pre-commit config.")
         delete_entity(Path(".pre-commit-config.yaml"))
-        print('Pre-commit config deleted.')
-             
+        print("Pre-commit config deleted.")
 
-def delete_entity(entity:Path()) -> None:
+
+def delete_entity(entity: Path()) -> None:
     print(f"   {entity}")
     if entity.is_file():
         os.remove(entity)
@@ -29,6 +32,5 @@ def delete_entity(entity:Path()) -> None:
         raise FileNotFoundError("File or directory does not exists.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
